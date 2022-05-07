3fc2644ab1c0d4f9acde0b1041c7551cb3e8f5e6
Regina Jackson <rjackson156@gmail.com>
2022-05-07 19:05:48 -0400

    reroute root and add resources for products

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  root to: 'home#home'
+  root to: 'products#index'
+  resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
