280c1e318f1fce61fd62c5e6e4de35a36adc1112
Dylan E. Richards <dylanerichards@gmail.com>
2022-05-16 21:41:30 -0400

    Add new columns to product for description and product type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_14_192019) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_17_014107) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_14_192019) do
     t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "product_type"
+    t.string "description"
   end
 
   create_table "users", force: :cascade do |t|
