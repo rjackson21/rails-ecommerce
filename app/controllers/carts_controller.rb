9cf182061c8abed96527c56246bc00e98ea42a12
Dylan E. Richards <dylanerichards@gmail.com>
2022-05-25 00:43:04 -0400

    Add cart subtotal

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,7 +3,8 @@ class CartsController < ApplicationController
     cart = current_user.cart
 
     @products = cart.products
-
     @cart_products = CartProduct.where(product_id: cart.products.map(&:id))
+    
+    @cart_subtotal = @cart_products.map { |cp| cp.quantity * cp.product.price }.sum
   end
 end
