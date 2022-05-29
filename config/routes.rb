52ad3db3e35f3cc1a99d8f91fbb164bf26c38e10
Dylan Richards <dylanerichards@gmail.com>
2022-05-29 16:54:42 -0400

    Show user information in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  get 'cart_products/create'
-  get 'carts/show'
+  root to: 'products#index'
+
   devise_for :users, :controllers => { :registrations => "registrations" }
 
-  resources :users do
-    resource :cart, only: [:show], on: :member
-  end
+  get "/cart", to: "carts#show"
+
+  resources :products
 
   resources :cart_products, only: [:create]
-
-  root to: 'products#index'
-  resources :products
 end
