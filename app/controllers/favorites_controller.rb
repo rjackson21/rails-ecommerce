888430d26b6e24b4f9963042a9e9652516fabbb4
Dylan Richards <dylanerichards@gmail.com>
2022-05-29 17:51:42 -0400

    Formatting

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,6 +2,7 @@ class FavoritesController < ApplicationController
   def create
     if current_user
       favorite = Favorite.find_by(user_id: current_user.id, product_id: params[:product_id])
+
       if favorite
         favorite.destroy
       else
@@ -10,8 +11,6 @@ class FavoritesController < ApplicationController
           product_id: params[:product_id]
         )
       end
-
-
     else
       flash[:notice] = "You need to sign in to favorite products."
     end
